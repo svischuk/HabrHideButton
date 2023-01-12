764e53f8db59d7d04d525dce7787f9bc11861cfd
svischuk <svischuk@gmail.com>
2023-01-13 02:16:54 +0300

    Update HabrHideButton.meta.js

--- HabrHideButton.meta.js
+++ HabrHideButton.meta.js
@@ -6,8 +6,8 @@
 // @author       svischuk
 // @match        https://habr.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=habr.com
-// @downloadURL  https://github.com/svischuk/HabrHideButton/blob/main/HabrHideButton.user.js
-// @updateURL    https://github.com/svischuk/HabrHideButton/blob/main/HabrHideButton.meta.js
+// @downloadURL  https://github.com/svischuk/HabrHideButton/raw/main/HabrHideButton.user.js
+// @updateURL    https://github.com/svischuk/HabrHideButton/raw/main/HabrHideButton.meta.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_deleteValue
