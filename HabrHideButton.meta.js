d0905ca69a7a0594b01c51778467afd95444c739
svischuk <svischuk@gmail.com>
2023-01-13 02:09:25 +0300

    Create HabrHideButton.meta.js

---
+++ HabrHideButton.meta.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Hide button for Habr.com
+// @namespace    https://github.com/svischuk
+// @version      0.1
+// @description  Help to hide posts
+// @author       svischuk
+// @match        https://habr.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=habr.com
+// @downloadURL  https://github.com/svischuk/HabrHideButton/blob/main/HabrHideButton.js
+// @updateURL    https://github.com/svischuk/HabrHideButton/blob/main/HabrHideButton.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_getResourceText
+// @grant        GM_getResourceURL
+// @grant        GM_addStyle
+// @grant        GM_log
+// @grant        GM_setClipboard
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @grant        GM_info
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
