6e0773d5b2db5b5f03c533365647487d856332fc
svischuk <svischuk@gmail.com>
2023-01-13 01:33:04 +0300

    Update HabrHideButton.js

--- HabrHideButton.js
+++ HabrHideButton.js
@@ -6,7 +6,21 @@
 // @author       You
 // @match        https://habr.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=habr.com
-// @grant        none
+// @grant           property:settings
+// @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_deleteValue
+// @grant           GM_listValues
+// @grant           GM_getResourceText
+// @grant           GM_getResourceURL
+// @grant           GM_addStyle
+// @grant           GM_log
+// @grant           GM_setClipboard
+// @grant           GM_xmlhttpRequest
+// @grant           unsafeWindow
+// @grant           GM_info
+// @grant           GM_openInTab
+// @grant           GM_registerMenuCommand
 // ==/UserScript==
 
 const timeout = 10;
