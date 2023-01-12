add21f95dda2f8f5cac60cb393334545e3865077
svischuk <svischuk@gmail.com>
2023-01-13 02:29:50 +0300

    Update HabrHideButton.user.js

--- HabrHideButton.user.js
+++ HabrHideButton.user.js
@@ -12,16 +12,8 @@
 // @grant        GM_setValue
 // @grant        GM_deleteValue
 // @grant        GM_listValues
-// @grant        GM_getResourceText
-// @grant        GM_getResourceURL
 // @grant        GM_addStyle
-// @grant        GM_log
-// @grant        GM_setClipboard
 // @grant        GM_xmlhttpRequest
-// @grant        unsafeWindow
-// @grant        GM_info
-// @grant        GM_openInTab
-// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 const timeout = 10;
