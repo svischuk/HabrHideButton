c99127d766ca01c7e7b7e9fd3a861e88a55b2117
svischuk <svischuk@gmail.com>
2023-02-19 21:23:12 +0300

    article stats added

--- HabrHideButton.meta.js
+++ HabrHideButton.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide button for Habr.com
 // @namespace    https://github.com/svischuk
-// @version      0.1
+// @version      0.11
 // @description  Help to hide posts
 // @author       svischuk
 // @match        https://habr.com/*
