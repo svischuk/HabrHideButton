fb47c5c66e724ed307ac96a9b4201c2b6e1d6fd0
svischuk <svischuk@gmail.com>
2023-02-19 23:40:27 +0300

    header added

--- HabrHideButton.user.js
+++ HabrHideButton.user.js
@@ -1,4 +1,20 @@
-
+// ==UserScript==
+// @name         Hide button for Habr.com
+// @namespace    https://github.com/svischuk
+// @version      0.12
+// @description  Help to hide posts
+// @author       svischuk
+// @match        https://habr.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=habr.com
+// @downloadURL  https://github.com/svischuk/HabrHideButton/raw/main/HabrHideButton.user.js
+// @updateURL    https://github.com/svischuk/HabrHideButton/raw/main/HabrHideButton.meta.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
 const timeout = 100;
 const messages = {
     hide: {
